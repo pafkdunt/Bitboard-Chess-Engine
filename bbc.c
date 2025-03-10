852bab1de22a47c7e34e1549c00ff0424b513e7c
pafkdunt <sharma.rohan4@northeastern.edu>
2025-03-10 01:39:50 -0400

    setting up the board

--- bbc.c
+++ bbc.c
@@ -1,12 +1,33 @@
 // system headers
 #include<stdio.h>
 
+// define bitboard data type
+#define U64 unsigned long long
 
 /******************\
   BIT MANIPULATION
 \******************/
 
+// print bitboard
+void print_bitboard(U64 bitboard)
+{
+    // loop over board ranks
+    for (int rank = 0; rank < 8; rank++)
+    {
+        // loop over board files
+        for (int file = 0; file < 8; file++)
+        {
+            // convert file + rank -> square index
+            int square = rank * 8 + file; 
 
+            //
+            printf(" %d ", (bitboard & (1ULL << square)) ? 1 : 0);
+        }
+
+        // print new line every rank
+        printf("\n");
+    }
+}
 
 
 /*************\
@@ -15,6 +36,8 @@
 
 int main()
 {
-    printf("bitboard chess\n");
+    U64 bitboard = 4ULL;
+    print_bitboard(bitboard);
+
     return 0;
 }
